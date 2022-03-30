621e252deb0d1643654ca3d7bf4ea4a79bebbcd5
Abhay Sharma <86826529+ADS255@users.noreply.github.com>
2022-03-30 21:34:53 +0100

    string hashtable added

--- src/JSU/JSUData.java
+++ src/JSU/JSUData.java
@@ -5,4 +5,5 @@ import java.util.Hashtable;
 public class JSUData implements Serializable
 {
     public Hashtable <String, Integer> intDict = new Hashtable<>();
+    public Hashtable <String, String> stringDict = new Hashtable<>();
 }
